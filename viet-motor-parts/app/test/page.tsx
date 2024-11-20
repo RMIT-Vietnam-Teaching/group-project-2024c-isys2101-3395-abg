87079b406f4344afe24251f43149b7c7f9628278
Tôn Thất Hữu Luân <s3958304@rmit.edu.vn>
2024-11-20 12:20:34 +0700

    Add Product Dialog init
    Import shadcn dialog, button element
    Create Add Product Dialog element
    Add Test Page for Components

---
+++ viet-motor-parts/app/test/page.tsx
@@ -0,0 +1,12 @@
+import Dialogv2 from "../components/Dialogv2";
+import ProductDialog from "../components/ProductDialog";
+
+export default function Page() {
+    return (
+        <div>
+            <h1>Page for Component Testing</h1>
+            <ProductDialog />
+            <Dialogv2 />
+        </div>
+    );
+}
