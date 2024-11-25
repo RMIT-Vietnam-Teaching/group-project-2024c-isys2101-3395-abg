d01bf988e3742c18382547aea35fe7621cef8b37
Tôn Thất Hữu Luân <s3958304@rmit.edu.vn>
2024-11-25 23:50:27 +0700

    Compatible Vehicles search bar

--- viet-motor-parts/app/components/CompatibleVehicle.tsx
+++ viet-motor-parts/app/components/CompatibleVehicle.tsx
@@ -1,34 +1,64 @@
 "use client"
 
 import { useEffect, useState } from "react";
+import { Input } from "./shadcn/input";
+import { X } from "lucide-react";
 
 export default function CompatibleVehicle() {
     const [searchTerm, setSearchTerm] = useState('');
-    const [suggestions, setSuggestions] = useState<{ users: { email: string; firstName: string; lastName: string; }[] }>({ users: [] });
+    const [suggestions, setSuggestions] = useState<{ vehicles: { email: string; firstName: string; lastName: string; }[] }>({ vehicles: [] });
+    const [selectedVehicles, setSelectedVehicles] = useState<{ vehicles: { email: string; firstName: string; lastName: string; }[] }>({ vehicles: [] });
+    const [selectedVehicleSet, setSelectedVehicleSet] = useState<Set<string>>(new Set());
 
     const fetchSuggestions = async () => {
         if (searchTerm.trim() === '') {
-            setSuggestions({ users: [] });
+            setSuggestions({ vehicles: [] });
             return;
         }
         fetch(`https://dummyjson.com/users/search?q=${searchTerm}`)
             .then((res) => res.json())
-            .then((data) => { setSuggestions({ users: data.users }) })
+            .then((data) => { setSuggestions({ vehicles: data.users }) }) // replace data.users with data.vehicles
             .catch((err) => console.error(err));
     };
 
     useEffect(() => { fetchSuggestions() }, [searchTerm]);
+
+    const handleSelectVehicles = (vehicles: { email: string; firstName: string; lastName: string; }) => {
+        setSelectedVehicles({ vehicles: [...selectedVehicles.vehicles, vehicles] });
+        setSelectedVehicleSet(new Set([...selectedVehicleSet, vehicles.email]));
+        setSearchTerm('');
+        setSuggestions({ vehicles: [] });
+    };
+
+    console.log(selectedVehicles);
+
     return (
-        <div>
+        <div className="col-span-3 flex flex-col gap-2">
             <div>
-                <div>
-                    <input type="text" name="" id="" value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} placeholder="Search for compatbile Vehicles" />
-                </div>
-                <ul>
-                    {suggestions.users.map((user, index) => {
-                        return <li key={user.email}><span>{user.firstName} {user.lastName}</span></li>;
-                    })}
-                </ul>
+                <Input className="text-black w-full bg-white placeholder:text-black" type="text" name="" id="" value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} placeholder="Search for compatible vehicles" />
+            </div>
+            <ul className="overflow-y-scroll max-h-[150px]">
+                {suggestions.vehicles.map((vehicle, index) => {
+                    return !selectedVehicleSet.has(vehicle.email) ? (
+                        <li key={vehicle.email} onClick={() => handleSelectVehicles(vehicle)}>
+                            <div className=" p-2 bg-brand-300 text-white hover:bg-brand-500 border-b-2 hover:cursor-pointer">
+                                <h3>{vehicle.firstName} {vehicle.lastName}</h3>
+                            </div>
+                        </li>) : null;
+                })}
+            </ul>
+            <div className="flex flex-row gap-3">
+                {selectedVehicles.vehicles.map((vehicle, index) => {
+                    return (
+                        <span key={vehicle.email} className="bg-white text-black p-1 rounded-lg flex flex-row items-center gap-2">
+                            <h3 className="font-bold">{vehicle.firstName} {vehicle.lastName}</h3>
+                            <X className="w-4 h-4 hover:text-red-600 rounded-lg hover:cursor-pointer" onClick={() => {
+                                setSelectedVehicles({ vehicles: selectedVehicles.vehicles.filter((v) => v.email !== vehicle.email) });
+                                setSelectedVehicleSet(new Set([...selectedVehicleSet].filter((email) => email !== vehicle.email)));
+                            }} />
+                        </span>
+                    );
+                })}
             </div>
         </div>
     );
