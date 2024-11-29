a4ac9b1e223d8717e240ea12718b08397b79c86a
Tôn Thất Hữu Luân <s3958304@rmit.edu.vn>
2024-11-29 16:16:04 +0700

    Update page.tsx

--- viet-motor-parts/app/(default)/products/page.tsx
+++ viet-motor-parts/app/(default)/products/page.tsx
@@ -33,13 +33,14 @@ export default async function Page({ searchParams }: { searchParams: Record<stri
     return (
         <section className="flex flex-col w-full gap-10 pb-10 my-5 content lg:flex-row px-7">
             <aside
-                className="left-0 w-full p-5 border rounded-lg bg-palette-3 md:max-lg:block lg:block lg:sticky top-10 h-5/6 lg:w-1/4 md:max-lg:center-and-half">
+                className="left-0 w-full rounded-lg bg-palette-3 md:max-lg:block lg:block lg:sticky top-10 h-5/6 lg:w-1/4 md:max-lg:center-and-half">
+                <SideFilter />
             </aside>
             <div className="flex flex-col justify-center w-full xl:w-3/4">
                 <section
                     className="grid items-center justify-center grid-cols-1 gap-3 mb-5 md:grid-cols-2 min-[1440px]:grid-cols-3 justify-items-center md:max-w-2xl lg:max-w-full lg:mx-0">
                     {products.map((product: Product) => (
-                        <ProductCard key={product._id} _id={product._id} name={product.name} price={product.price} image_base64={product.image_base64 || ""} description={product.description} brand={product.brand} compatible_vehicles={product.compatible_vehicles} stock_quantity={product.stock_quantity} category_id={product.category_id}  />
+                        <ProductCard key={product._id} _id={product._id} name={product.name} price={product.price} image_base64={product.image_base64 || ""} description={product.description} brand={product.brand} compatible_vehicles={product.compatible_vehicles} stock_quantity={product.stock_quantity} category_id={product.category_id} />
                     ))}
                 </section>
                 <div>
