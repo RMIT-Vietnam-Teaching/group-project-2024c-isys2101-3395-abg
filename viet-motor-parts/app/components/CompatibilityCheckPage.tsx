49e4c78f9146650fdf08aacfaa56a75a11e4ef81
s3986423 <s3986423@rmit.edu.vn>
2024-12-10 17:54:25 +0700

    Update the Compatibility Check Page
    - Implement actual compatibility check logic
    - Update the svg(s) into using luicide-react components

--- viet-motor-parts/app/components/CompatibilityCheckPage.tsx
+++ viet-motor-parts/app/components/CompatibilityCheckPage.tsx
@@ -1,18 +1,39 @@
 "use client";
 
 import { useState } from "react";
-import { Product } from '@/app/components/ProductCard';
-import SearchBarCompatibility from "@/app/components/SearchBarCompatibility";
+import { CircleCheck, CirclePlus, Link } from 'lucide-react';
+import SearchBarCompatibility, { CompatibleVehicle } from "@/app/components/SearchBarCompatibility";
+
+export interface Product {
+    _id: string;
+    name: string;
+    price: number;
+    image_base64: string;
+    description: string;
+    brand: string;
+    stock_quantity: number;
+    category_id: string;
+    compatible_vehicles: Array<string>;
+}
+
+type SelectedItem = Product | CompatibleVehicle | null;
 
 export default function CompatabilityCheckPage(){
-    const [selectedVehicle, setSelectedVehicle] = useState<Product | null>();
-    const [selectedProduct, setSelectedProduct] = useState<Product | null>();
+    const [selectedVehicle, setSelectedVehicle] = useState<SelectedItem>(null);
+    const [selectedProduct, setSelectedProduct] = useState<SelectedItem>(null);
     const [isCompatible, setIsCompatible] = useState<boolean | null>(null);
 
     const handleCheckCompatibility = () => {
-        // Replace this with compatibility check logic later
-        const compatible = true;
-        setIsCompatible(compatible);
+        if (selectedProduct && selectedVehicle) {
+            const product = selectedProduct as Product;
+            const vehicle = selectedVehicle as CompatibleVehicle;
+
+            const compatible = product.compatible_vehicles.includes(vehicle._id);
+
+            setIsCompatible(compatible);
+        } else {
+            setIsCompatible(false);
+        }
     };
 
     const isButtonDisabled = selectedVehicle == null || selectedProduct == null;
@@ -36,32 +57,19 @@ export default function CompatabilityCheckPage(){
                     } text-brand-100 focus:ring-4 focus:outline-none focus:ring-brand-100 font-medium rounded-lg text-sm px-4 py-2 text-center flex justify-start items-center`}
             >
                 Check Compatibility
-                <svg className="h-6 w-6 ml-3 md:ml-0" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
-                    <path stroke="none" d="M0 0h24v24H0z" />
-                    <path d="M10 14a3.5 3.5 0 0 0 5 0l4 -4a3.5 3.5 0 0 0 -5 -5l-.5 .5" />
-                    <path d="M14 10a3.5 3.5 0 0 0 -5 0l-4 4a3.5 3.5 0 0 0 5 5l.5 -.5" />
-                    <line x1="16" y1="21" x2="16" y2="19" />
-                    <line x1="19" y1="16" x2="21" y2="16" />
-                    <line x1="3" y1="8" x2="5" y2="8" />
-                    <line x1="8" y1="3" x2="8" y2="5" />
-                </svg>
+                <Link size={25} strokeWidth={1.5}/>
             </button>
         </div>
 
-
         {/* Right Section */}
         <div className="col-span-1 md:col-span-3">
-            <SearchBarCompatibility barType="products" onSelect={setSelectedProduct}/>                
+            <SearchBarCompatibility barType="products" onSelect={setSelectedProduct}/>      
         </div>
         {isCompatible !== null && (
         isCompatible ? 
         (<div className="col-span-1 md:col-span-7 flex justify-center items-center md:order-none order-12 rounded-2xl">
             <div className="flex flex-col md:flex-row items-center bg-brand-600 rounded p-3">
-                <svg className="h-20 w-20 text-brand-100" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                    <path stroke="none" d="M0 0h24v24H0z" />
-                    <circle cx="12" cy="12" r="9" />
-                    <path d="M9 12l2 2l4 -4" />
-                </svg>
+                <CircleCheck size={100} className="text-brand-100"/>
                 <div className="ml-3 text-center md:text-left">
                     <h1 className="text-brand-100 text-xl md:text-2xl font-bold mb-2">Compatible!</h1>
                     <h2 className="text-brand-100 text-sm md:text-lg font-bold">The chosen vehicle and part are compatible!</h2>
@@ -70,9 +78,7 @@ export default function CompatabilityCheckPage(){
         </div>) :
         (<div className="col-span-1 md:col-span-7 flex justify-center items-center md:order-none order-12 rounded-2xl">
             <div className="flex flex-col md:flex-row items-center bg-brand-100 rounded p-3">
-                <svg className="h-20 w-20 text-brand-600" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
-                </svg>
+                <CirclePlus size={100} className="text-brand-600" style={{ transform: 'rotate(45deg)' }}/>
                 <div className="ml-3 text-center md:text-left">
                     <h1 className="text-brand-600 text-xl md:text-2xl font-bold mb-2">Not compatible!</h1>
                     <h2 className="text-brand-600 text-sm md:text-lg font-bold">The chosen vehicle and part are not compatible!</h2>
