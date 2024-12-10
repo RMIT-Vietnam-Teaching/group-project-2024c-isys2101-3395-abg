3d060c18c088b5abcb8918e4aa2cec120d4f6607
Vinh Gia Bao Nguyen <s3986287@rmit.edu.vn>
2024-12-11 00:01:03 +0700

    Update page.tsx

--- viet-motor-parts/app/(default)/products/[id]/edit/page.tsx
+++ viet-motor-parts/app/(default)/products/[id]/edit/page.tsx
@@ -1,13 +1,6 @@
 import ProductEditForm from "./ProductEditForm";
-import CompatibleVehicle from "@/app/components/CompatibleVehicle";
-import CurrencyInputVietnam from "@/app/components/CurrencyInputVietnam";
-import { Button } from "@/app/components/shadcn/button";
-import { Input } from "@/app/components/shadcn/input";
-import { Label } from "@/app/components/shadcn/label";
-import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/app/components/shadcn/select";
-import { Textarea } from "@/app/components/shadcn/textarea";
 import { fetchProductbyID } from "../fetchProductbyID";
-import { Metadata } from "next";
+
 
 export async function generateMetadata({ params }: { params: { id: string } }) {
   const res = await fetchProductbyID(params.id);
