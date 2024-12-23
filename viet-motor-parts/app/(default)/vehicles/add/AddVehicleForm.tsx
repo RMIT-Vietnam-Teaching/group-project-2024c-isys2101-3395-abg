66978559b4c515edf581e76b0f03b9e839622bca
Tôn Thất Hữu Luân <s3958304@rmit.edu.vn>
2024-12-24 00:58:57 +0700

    Update AddVehicleForm.tsx
    Fix async function

--- viet-motor-parts/app/(default)/vehicles/add/AddVehicleForm.tsx
+++ viet-motor-parts/app/(default)/vehicles/add/AddVehicleForm.tsx
@@ -13,7 +13,7 @@ export default function AddVehicleForm() {
         const make = formData.get("make") as string;
         const vehicleModel = formData.get("model") as string;
         const year = formData.get("year") as string;
-        const token = getAuthToken();
+        const token = await getAuthToken();
 
         if (!token) {
             console.error("No token found. Please log in.");
@@ -47,43 +47,43 @@ export default function AddVehicleForm() {
     }
 
     return (<form action={handleSubmit}>
-                <div className="grid gap-4 py-4">
-                    <div className="grid grid-row-2 lg:grid-cols-4 items-center  gap-2 lg:gap-4">
-                        <Label htmlFor="make" className="text-left lg:text-right font-bold">
-                            Make
-                        </Label>
-                        <Input
-                            id="make" name="make" type="text"
-                            className="col-span-3 rounded-md p-2 bg-white text-black focus:outline-none"
-                            placeholder="e.g. Honda"
-                        />
-                    </div>
-                    <div className="grid grid-row-2 lg:grid-cols-4 items-center  gap-2 lg:gap-4">
-                        <Label htmlFor="model" className="text-left lg:text-right font-bold">
-                            Model
-                        </Label>
-                        <Input
-                            id="model" name="model" type="text"
-                            className="col-span-3 rounded-md p-2 bg-white text-black focus:outline-none"
-                            placeholder="e.g. Vision"
-                        />
-                    </div>
-                    <div className="grid grid-row-2 lg:grid-cols-4 items-center  gap-2 lg:gap-4">
-                        <Label htmlFor="year" className="text-left lg:text-right font-bold">
-                            Year
-                        </Label>
-                        <Input
-                            id="year" name="year" type="number"
-                            className="col-span-3 rounded-md p-2 bg-white text-black focus:outline-none"
-                            min={1900}
-                            max={new Date().getFullYear() + 1}
-                            placeholder="e.g. 2020"
-                        />
-                    </div>
-                    <div className="flex justify-end">
-                        <Button className="rounded-lg bg-gradient-to-r from-brand-300 via-brand-400 to-brand-600 px-5 py-2.5 text-center text-sm font-bold text-white hover:bg-gradient-to-bl">Add Vehicle</Button>
-                    </div>
-                </div>
-            </form>
+        <div className="grid gap-4 py-4">
+            <div className="grid grid-row-2 lg:grid-cols-4 items-center  gap-2 lg:gap-4">
+                <Label htmlFor="make" className="text-left lg:text-right font-bold">
+                    Make
+                </Label>
+                <Input
+                    id="make" name="make" type="text"
+                    className="col-span-3 rounded-md p-2 bg-white text-black focus:outline-none"
+                    placeholder="e.g. Honda"
+                />
+            </div>
+            <div className="grid grid-row-2 lg:grid-cols-4 items-center  gap-2 lg:gap-4">
+                <Label htmlFor="model" className="text-left lg:text-right font-bold">
+                    Model
+                </Label>
+                <Input
+                    id="model" name="model" type="text"
+                    className="col-span-3 rounded-md p-2 bg-white text-black focus:outline-none"
+                    placeholder="e.g. Vision"
+                />
+            </div>
+            <div className="grid grid-row-2 lg:grid-cols-4 items-center  gap-2 lg:gap-4">
+                <Label htmlFor="year" className="text-left lg:text-right font-bold">
+                    Year
+                </Label>
+                <Input
+                    id="year" name="year" type="number"
+                    className="col-span-3 rounded-md p-2 bg-white text-black focus:outline-none"
+                    min={1900}
+                    max={new Date().getFullYear() + 1}
+                    placeholder="e.g. 2020"
+                />
+            </div>
+            <div className="flex justify-end">
+                <Button className="rounded-lg bg-gradient-to-r from-brand-300 via-brand-400 to-brand-600 px-5 py-2.5 text-center text-sm font-bold text-white hover:bg-gradient-to-bl">Add Vehicle</Button>
+            </div>
+        </div>
+    </form>
     )
 }
