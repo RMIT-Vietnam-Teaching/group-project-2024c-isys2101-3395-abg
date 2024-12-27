24bdaaf26dabcceeeb8629e22e4c3bdea0c81320
Vinh Gia Bao Nguyen <s3986287@rmit.edu.vn>
2024-12-27 20:04:19 +0700

    delete the type="submit" in this update

--- viet-motor-parts/app/(default)/products/add/page.tsx
+++ viet-motor-parts/app/(default)/products/add/page.tsx
@@ -1,14 +1,4 @@
 import { fetchCategories } from "@/app/(default)/categories/fetchCategories";
-import { Label } from "@/app/components/shadcn/label";
-import { Input } from "@/app/components/shadcn/input";
-import { Textarea } from "@/app/components/shadcn/textarea";
-import CurrencyInputVietnam from "@/app/components/CurrencyInputVietnam";
-import ImageInput from "@/app/components/ImageInput";
-import CompatibleVehicle from "@/app/components/CompatibleVehicle";
-import { Button } from "@/app/components/shadcn/button";
-import { createProducts } from "./createProducts";
-import { redirect } from "next/navigation";
-import { revalidatePath } from "next/cache";
 import AddProductForm from "./form";
 
 export async function generateMetadata() {
