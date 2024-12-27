7a415be51953232d5b6183ddde65d4cfaa875cad
Vinh Gia Bao Nguyen <s3986287@rmit.edu.vn>
2024-12-27 17:57:35 +0700

    Add "Delete Product" button in edit form

--- viet-motor-parts/app/(default)/products/[id]/edit/ProductEditForm.tsx
+++ viet-motor-parts/app/(default)/products/[id]/edit/ProductEditForm.tsx
@@ -15,6 +15,7 @@ import { CircleXIcon } from "lucide-react";
 import { Vehicle } from "@/app/(default)/vehicles/fetchVehicles";
 import { updateProduct } from "./updateProducts";
 import { useRouter } from "next/navigation";
+import { getAuthToken } from "@/lib/auth";
 
 type ProductEditFormProps = {
   product: Product;
@@ -22,9 +23,14 @@ type ProductEditFormProps = {
   categories: Category[];
 };
 
-export default function ProductEditForm({ product, compatibleVehicles, categories }: ProductEditFormProps) {
+export default function ProductEditForm({
+  product,
+  compatibleVehicles,
+  categories,
+}: ProductEditFormProps) {
   const router = useRouter();
   const [error, formAction] = useFormState(editProduct, null); // Hook to display error message
+  const [deleteError, deleteFormAction] = useFormState(handleDelete, null);
 
   async function editProduct(state: any, formData: FormData) {
     const name = formData.get("name") as string;
@@ -54,112 +60,164 @@ export default function ProductEditForm({ product, compatibleVehicles, categorie
     }
   }
 
+  async function handleDelete() {
+    const token = await getAuthToken();
+
+    if (!token) {
+      console.error("No token found. Please log in.");
+      return;
+    }
+
+    try {
+      const res = await fetch(
+        `http://localhost:3000/api/products/${product._id}`,
+        {
+          method: "DELETE",
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        },
+      );
+
+      const data = await res.json();
+
+      if (!res.ok) {
+        return data.error;
+      }
+
+      // Navigate back to the vehicles page and reload
+      router.push("/products");
+      router.refresh(); // This will reload the page to ensure updated data is displayed
+    } catch (error) {
+      return error;
+    }
+  }
 
   return (
-    <form action={formAction} className="space-y-4">
-      <div className="grid-row-2 grid items-center gap-2 lg:grid-cols-4 lg:gap-4">
-        <Label htmlFor="name" className="text-left font-bold lg:text-right">
-          Name
-        </Label>
-        <Input
-          id="name"
-          name="name"
-          type="text"
-          className="col-span-3 bg-white text-black"
-          required
-          defaultValue={product.name}
-        />
-      </div>
-      <div className="grid-row-2 grid items-center gap-2 lg:grid-cols-4 lg:gap-4">
-        <Label htmlFor="brand" className="text-left font-bold lg:text-right">
-          Brand
-        </Label>
-        <Input
-          id="brand"
-          name="brand"
-          type="text"
-          className="col-span-3 bg-white text-black"
-          required
-          defaultValue={product.brand}
-        />
-      </div>
-      <div className="grid-row-2 grid items-center gap-2 lg:grid-cols-4 lg:gap-4">
-        <Label
-          htmlFor="description"
-          className="text-left font-bold lg:text-right"
-        >
-          Description
-        </Label>
-        <Textarea
-          id="description"
-          name="description"
-          className="col-span-3 bg-white text-black"
-          required
-          minLength={10}
-          defaultValue={product.description}
-        />
-      </div>
-      <div className="grid-row-2 grid items-center gap-2 lg:grid-cols-4 lg:gap-4">
-        <Label htmlFor="price" className="text-left font-bold lg:text-right">
-          Price (VNĐ)
-        </Label>
-        <CurrencyInputVietnam
-          name="price"
-          defaultValue={product.price}
-        />
-      </div>
-      <div className="grid grid-cols-1 gap-2 lg:grid-cols-4 lg:gap-4">
-        <Label htmlFor="image" className="text-left font-bold lg:text-right">
-          Image
-        </Label>
-        <ClientImageInput name="image" defaultValue={getProductImage(product.image_base64)} />
-      </div>
-      <div className="grid-row-2 grid items-center gap-2 lg:grid-cols-4 lg:gap-4">
-        <Label htmlFor="quantity" className="text-left font-bold lg:text-right">
-          Stock Quantity
-        </Label>
-        <Input
-          id="quantity"
-          name="stock_quantity"
-          type="number"
-          min="0"
-          className="col-span-3 bg-white text-black"
-          required
-          defaultValue={product.stock_quantity}
-        />
-      </div>
-      <div className="grid-row-2 grid items-center gap-2 lg:grid-cols-4 lg:gap-4">
-        <Label
-          htmlFor="category_id"
-          className="text-left font-bold lg:text-right"
-        >
-          Category
-        </Label>
-        <select
-          className="select col-span-3 bg-white text-black"
-          name="category_id"
-          id="category_id"
-          defaultValue={product.category_id}
-        >
-          {categories.map((category) => (
-            <option key={category._id} value={category._id}>
-              {category.name}
-            </option>
-          ))}
-        </select>
-      </div>
-      <div className="grid-row-2 grid items-center gap-2 lg:grid-cols-4 lg:gap-4">
-        <Label
-          htmlFor="compatibleVehicles"
-          className="text-left font-bold lg:text-right"
-        >
-          Compatible Vehicles
-        </Label>
-        <CompatibleVehicle vehicles={compatibleVehicles} />
-      </div>
-      <div className="flex justify-end">
-        <SubmitButton />
-      </div>
+    <div>
+      <form action={formAction} className="space-y-4">
+        <div className="grid-row-2 grid items-center gap-2 lg:grid-cols-4 lg:gap-4">
+          <Label htmlFor="name" className="text-left font-bold lg:text-right">
+            Name
+          </Label>
+          <Input
+            id="name"
+            name="name"
+            type="text"
+            className="col-span-3 bg-white text-black"
+            required
+            defaultValue={product.name}
+          />
+        </div>
+        <div className="grid-row-2 grid items-center gap-2 lg:grid-cols-4 lg:gap-4">
+          <Label htmlFor="brand" className="text-left font-bold lg:text-right">
+            Brand
+          </Label>
+          <Input
+            id="brand"
+            name="brand"
+            type="text"
+            className="col-span-3 bg-white text-black"
+            required
+            defaultValue={product.brand}
+          />
+        </div>
+        <div className="grid-row-2 grid items-center gap-2 lg:grid-cols-4 lg:gap-4">
+          <Label
+            htmlFor="description"
+            className="text-left font-bold lg:text-right"
+          >
+            Description
+          </Label>
+          <Textarea
+            id="description"
+            name="description"
+            className="col-span-3 bg-white text-black"
+            required
+            minLength={10}
+            defaultValue={product.description}
+          />
+        </div>
+        <div className="grid-row-2 grid items-center gap-2 lg:grid-cols-4 lg:gap-4">
+          <Label htmlFor="price" className="text-left font-bold lg:text-right">
+            Price (VNĐ)
+          </Label>
+          <CurrencyInputVietnam name="price" defaultValue={product.price} />
+        </div>
+        <div className="grid grid-cols-1 gap-2 lg:grid-cols-4 lg:gap-4">
+          <Label htmlFor="image" className="text-left font-bold lg:text-right">
+            Image
+          </Label>
+          <ClientImageInput
+            name="image"
+            defaultValue={getProductImage(product.image_base64)}
+          />
+        </div>
+        <div className="grid-row-2 grid items-center gap-2 lg:grid-cols-4 lg:gap-4">
+          <Label
+            htmlFor="quantity"
+            className="text-left font-bold lg:text-right"
+          >
+            Stock Quantity
+          </Label>
+          <Input
+            id="quantity"
+            name="stock_quantity"
+            type="number"
+            min="0"
+            className="col-span-3 bg-white text-black"
+            required
+            defaultValue={product.stock_quantity}
+          />
+        </div>
+        <div className="grid-row-2 grid items-center gap-2 lg:grid-cols-4 lg:gap-4">
+          <Label
+            htmlFor="category_id"
+            className="text-left font-bold lg:text-right"
+          >
+            Category
+          </Label>
+          <select
+            className="select col-span-3 bg-white text-black"
+            name="category_id"
+            id="category_id"
+            defaultValue={product.category_id}
+          >
+            {categories.map((category) => (
+              <option key={category._id} value={category._id}>
+                {category.name}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div className="grid-row-2 grid items-center gap-2 lg:grid-cols-4 lg:gap-4">
+          <Label
+            htmlFor="compatibleVehicles"
+            className="text-left font-bold lg:text-right"
+          >
+            Compatible Vehicles
+          </Label>
+          <CompatibleVehicle vehicles={compatibleVehicles} />
+        </div>
+        <div className="flex gap-3 justify-end">
+          <SubmitButton />
+        </div>
+        {error ? (
+          <div className="flex justify-center">
+            <div role="alert" className="alert alert-error">
+              <CircleXIcon />
+              <span>{error}</span>
+            </div>
+          </div>
+        ) : null}
+      </form>
+      <form
+        action={deleteFormAction}
+        className="flex justify-end"
+        id="deleteProduct"
+      >
+        <DeleteButton />
+      </form>
       {error ? (
         <div className="flex justify-center">
           <div role="alert" className="alert alert-error">
@@ -167,16 +225,44 @@ export default function ProductEditForm({ product, compatibleVehicles, categorie
             <span>{error}</span>
           </div>
         </div>
-      ) : null}
-    </form>
+      ) : (
+        <></>
+      )}
+      {deleteError ? (
+        <div className="flex justify-center">
+          <div role="alert" className="alert alert-error">
+            <CircleXIcon />
+            <span>{deleteError}</span>
+          </div>
+        </div>
+      ) : (
+        <></>
+      )}
+    </div>
   );
 }
 
 function SubmitButton() {
   const { pending } = useFormStatus();
   return (
-    <Button className="rounded-lg bg-gradient-to-r from-brand-300 via-brand-400 to-brand-600 px-5 py-2.5 text-center text-sm font-bold text-white hover:bg-gradient-to-bl" disabled={pending}>
+    <Button
+      className="rounded-lg bg-gradient-to-r from-brand-300 via-brand-400 to-brand-600 px-5 py-2.5 text-center text-sm font-bold text-white hover:bg-gradient-to-bl"
+      disabled={pending}
+    >
       {pending ? "Submitting" : "Edit Product"}
+    </Button>
+  );
+}
+
+function DeleteButton() {
+  const { pending } = useFormStatus();
+  return (
+    <Button
+      type="submit"
+      className="rounded-lg bg-gradient-to-r from-brand-300 via-brand-400 to-brand-600 my-2.5 px-5 py-2.5 text-center text-sm font-bold text-white hover:bg-gradient-to-bl"
+      disabled={pending}
+    >
+      {pending ? "Deleting" : "Delete Product"}
     </Button>
   );
 }
