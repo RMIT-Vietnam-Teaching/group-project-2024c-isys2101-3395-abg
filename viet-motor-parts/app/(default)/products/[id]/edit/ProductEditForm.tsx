cc4f5b6bdd0b02fffc8a82c22d4d4debf11ff20b
Vinh Gia Bao Nguyen <s3986287@rmit.edu.vn>
2024-12-27 18:34:50 +0700

    Update ProductEditForm.tsx

--- viet-motor-parts/app/(default)/products/[id]/edit/ProductEditForm.tsx
+++ viet-motor-parts/app/(default)/products/[id]/edit/ProductEditForm.tsx
@@ -246,6 +246,7 @@ function SubmitButton() {
   const { pending } = useFormStatus();
   return (
     <Button
+      type="submit"
       className="rounded-lg bg-gradient-to-r from-brand-300 via-brand-400 to-brand-600 px-5 py-2.5 text-center text-sm font-bold text-white hover:bg-gradient-to-bl"
       disabled={pending}
     >
